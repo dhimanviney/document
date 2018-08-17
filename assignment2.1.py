c961fb7adf11866e7a739aef714dfd00acf5205c
dhimanviney <dhimanviney986@gmail.com>
2018-08-17 17:36:33 +0530

    hello

---
+++ assignment2.1.py
@@ -0,0 +1,23 @@
+#PRINTING ANYTHING ON SCREEN!!
+print('I am Learning Python!!');
+#CONCATING TWO STRING using'+'.
+print("New"+"Delhi");
+#Taking the input of 3 variables x,y,z from user and printing their values on the screen...
+x=input("enter first number :");
+y=input("enter second number :");
+z=input("enter third number :");
+print(x);
+print(y);
+print(z);
+#Printing "Let's get started" on screen!!
+print("Let's get started");
+#Printing the values using placeholder!!
+s="Acadview"
+course="python"
+fees=5000
+print("{} {} {}".format(s,course,fees));
+#Printing Area of Circle
+PI = 3.14
+r = float(input('Enter the radius of the circle :'))
+area = PI * r * r
+print('area of circle %.2f'%area)
